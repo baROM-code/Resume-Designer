d32358ee067a9c1f69f18f7c89762a498351a9f0
Roman Bar <barom64@mail.ru>
2022-07-05 15:04:33 +0400

    Подправил V1__init.sql и добавил данные в таблицы

--- core-service/src/main/resources/db/migration/V1__init.sql
+++ core-service/src/main/resources/db/migration/V1__init.sql
@@ -12,7 +12,7 @@ create table users
     updated_at    timestamp default current_timestamp
 );
 
-create table working
+create table works
 (
     id           bigserial primary key,
     user_id      bigint      not null references users (id),
@@ -25,7 +25,7 @@ create table working
     updated_at   timestamp default current_timestamp
 );
 
-create table education
+create table educations
 (
     id           bigserial primary key,
     user_id      bigint       not null references users (id),
@@ -36,7 +36,7 @@ create table education
     updated_at   timestamp default current_timestamp
 );
 
-create table resume
+create table resumes
 (
     id           bigserial primary key,
     user_id      bigint       not null references users (id),
@@ -48,5 +48,13 @@ create table resume
     updated_at   timestamp default current_timestamp
 );
 
+insert into users (id, userfirstname, userlastname, password, gender, dateofbirth, email) values
+(1, 'Иван', 'Иванов', '', 'M', '1980-01-01','ivan@email.ru');
 
+insert into works (user_id, organization, post, startwork, endwork, progress) values
+(1, 'R&k', 'Системный администратор', '2000-01-01', '2010-01-01', 'Много работал'),
+(1, '1C', 'Программист', '2011-01-01', '2015-01-01', 'Обновлял и редактировал конфигурацию 1С, делал формы отчетов');
+
+insert into educations (user_id, organization, speciality, year_end) values
+(1, 'МГУ', 'информационные технологии', '2002-07-01');
 
