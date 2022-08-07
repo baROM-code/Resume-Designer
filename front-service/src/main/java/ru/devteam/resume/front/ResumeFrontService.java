8df29e4b2e72e098ebad12b68d8dd1d17fb65439
Roman Bar <barom64@mail.ru>
2022-08-07 23:16:05 +0400

    миниправка

--- front-service/src/main/java/ru/devteam/resume/front/ResumeFrontService.java
+++ front-service/src/main/java/ru/devteam/resume/front/ResumeFrontService.java
@@ -1,13 +1,13 @@
-package ru.devteam;
+package ru.devteam.resume.front;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FrontService {
+public class ResumeFrontService {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FrontService.class, args);
+		SpringApplication.run(ResumeFrontService.class, args);
 	}
 
 }
